1fede40390900c634c112a9a4fa9e24b354231d9
Hayes Barber <barberhayes@gmail.com>
2025-09-17 08:22:31 -0400

    add helper fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import srl.commands
 from rich.console import Console
 from dataclasses import dataclass
 import pathlib
+import json
 
 
 @dataclass
@@ -37,3 +38,22 @@ def mock_data(tmp_path, monkeypatch):
         monkeypatch.setattr(f"srl.storage.{name}", path)
 
     yield paths
+
+
+@pytest.fixture
+def dump_json():
+    def _dump(path, data):
+        with open(path, "w") as f:
+            json.dump(data, f)
+
+    return _dump
+
+
+@pytest.fixture
+def load_json():
+    def _load(path):
+        with open(path) as f:
+            data = json.load(f)
+        return data
+
+    return _load

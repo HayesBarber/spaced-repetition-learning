f5e70608d6bf09335ee715efb47cd90a69371c59
Hayes Barber <barberhayes@gmail.com>
2025-09-19 07:51:48 -0400

    fix

--- tests/test_commands/test_audit.py
+++ tests/test_commands/test_audit.py
@@ -40,7 +40,7 @@ def test_show_current_audit(console):
     audit.handle(args=args, console=console)
 
     output = console.export_text()
-    assert f"Current audit problem" in output
+    assert "Current audit problem" in output
     assert problem in output
     assert "Run with --pass or --fail" in output
 

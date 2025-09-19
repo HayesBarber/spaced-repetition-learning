91844a521d5e8308eb29ff722e88dd1ffcef6809
Hayes Barber <barberhayes@gmail.com>
2025-09-19 08:54:21 -0400

    number of problems in mastered table

--- srl/commands/mastered.py
+++ srl/commands/mastered.py
@@ -8,14 +8,16 @@ from srl.storage import (
 
 def handle(args, console: Console):
     mastered_problems = get_mastered_problems()
+    mastered_count = len(mastered_problems)
     if args.c:
-        mastered_count = len(mastered_problems)
         console.print(f"[bold green]Mastered Count:[/bold green] {mastered_count}")
     else:
         if not mastered_problems:
             console.print("[yellow]No mastered problems yet.[/yellow]")
         else:
-            table = Table(title="Mastered Problems", title_justify="left")
+            table = Table(
+                title=f"Mastered Problems ({mastered_count})", title_justify="left"
+            )
             table.add_column("Problem", style="cyan", no_wrap=True)
             table.add_column("Attempts", style="magenta")
 

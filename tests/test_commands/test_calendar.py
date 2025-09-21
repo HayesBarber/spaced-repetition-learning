55bc698c4dc54d790cab7ed77faa59df8995676c
Hayes Barber <barberhayes@gmail.com>
2025-09-21 10:57:01 -0400

    clean up code

--- tests/test_commands/test_calendar.py
+++ tests/test_commands/test_calendar.py
@@ -1,23 +1,21 @@
 from srl.commands import calendar
+import pytest
 
 
-def test_get_audit_dates(mock_data, dump_json):
-    mock_audit_data = {
+@pytest.fixture
+def audit_data_pass_fail():
+    return {
         "history": [
             {"date": "2024-06-06", "problem": "problem3", "result": "pass"},
             {"date": "2024-06-07", "problem": "problem3", "result": "fail"},
             {"date": "2024-06-08", "problem": "problem3", "result": "pass"},
         ]
     }
-    dump_json(mock_data.AUDIT_FILE, mock_audit_data)
-
-    result = calendar.get_audit_dates()
-
-    assert result == ["2024-06-06", "2024-06-08"]
 
 
-def test_get_dates(mock_data, dump_json):
-    mock_mastered_data = {
+@pytest.fixture
+def mastered_data():
+    return {
         "problem1": {
             "history": [
                 {"rating": 3, "date": "2024-06-01"},
@@ -31,40 +29,11 @@ def test_get_dates(mock_data, dump_json):
             ]
         },
     }
-    dump_json(mock_data.MASTERED_FILE, mock_mastered_data)
-
-    result = calendar.get_dates(mock_data.MASTERED_FILE)
-
-    assert result == ["2024-06-01", "2024-06-02", "2024-06-01", "2024-06-03"]
 
 
-def test_get_all_date_counts(mock_data, dump_json):
-    mock_audit_data = {
-        "history": [
-            {"date": "2024-06-06", "problem": "problem3", "result": "pass"},
-            {"date": "2024-06-07", "problem": "problem3", "result": "fail"},
-            {"date": "2024-06-08", "problem": "problem3", "result": "pass"},
-        ]
-    }
-    dump_json(mock_data.AUDIT_FILE, mock_audit_data)
-
-    mock_mastered_data = {
-        "problem1": {
-            "history": [
-                {"rating": 3, "date": "2024-06-01"},
-                {"rating": 5, "date": "2024-06-02"},
-            ]
-        },
-        "problem2": {
-            "history": [
-                {"rating": 3, "date": "2024-06-01"},
-                {"rating": 4, "date": "2024-06-03"},
-            ]
-        },
-    }
-    dump_json(mock_data.MASTERED_FILE, mock_mastered_data)
-
-    mock_inprogress_data = {
+@pytest.fixture
+def inprogress_data():
+    return {
         "problem3": {
             "history": [
                 {"rating": 5, "date": "2024-06-04"},
@@ -72,7 +41,34 @@ def test_get_all_date_counts(mock_data, dump_json):
             ]
         }
     }
-    dump_json(mock_data.PROGRESS_FILE, mock_inprogress_data)
+
+
+def test_get_audit_dates(mock_data, dump_json, audit_data_pass_fail):
+    dump_json(mock_data.AUDIT_FILE, audit_data_pass_fail)
+
+    result = calendar.get_audit_dates()
+
+    assert result == ["2024-06-06", "2024-06-08"]
+
+
+def test_get_dates(mock_data, dump_json, mastered_data):
+    dump_json(mock_data.MASTERED_FILE, mastered_data)
+
+    result = calendar.get_dates(mock_data.MASTERED_FILE)
+
+    assert result == ["2024-06-01", "2024-06-02", "2024-06-01", "2024-06-03"]
+
+
+def test_get_all_date_counts(
+    mock_data,
+    dump_json,
+    audit_data_pass_fail,
+    mastered_data,
+    inprogress_data,
+):
+    dump_json(mock_data.AUDIT_FILE, audit_data_pass_fail)
+    dump_json(mock_data.MASTERED_FILE, mastered_data)
+    dump_json(mock_data.PROGRESS_FILE, inprogress_data)
 
     result = calendar.get_all_date_counts()
 

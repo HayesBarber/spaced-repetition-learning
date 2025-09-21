e2e3f24e35824d1d1ceb03e8fd91b99d0a4a4e62
Hayes Barber <barberhayes@gmail.com>
2025-09-21 10:52:55 -0400

    test_get_all_date_counts

--- tests/test_commands/test_calendar.py
+++ tests/test_commands/test_calendar.py
@@ -36,3 +36,51 @@ def test_get_dates(mock_data, dump_json):
     result = calendar.get_dates(mock_data.MASTERED_FILE)
 
     assert result == ["2024-06-01", "2024-06-02", "2024-06-01", "2024-06-03"]
+
+
+def test_get_all_date_counts(mock_data, dump_json):
+    mock_audit_data = {
+        "history": [
+            {"date": "2024-06-06", "problem": "problem3", "result": "pass"},
+            {"date": "2024-06-07", "problem": "problem3", "result": "fail"},
+            {"date": "2024-06-08", "problem": "problem3", "result": "pass"},
+        ]
+    }
+    dump_json(mock_data.AUDIT_FILE, mock_audit_data)
+
+    mock_mastered_data = {
+        "problem1": {
+            "history": [
+                {"rating": 3, "date": "2024-06-01"},
+                {"rating": 5, "date": "2024-06-02"},
+            ]
+        },
+        "problem2": {
+            "history": [
+                {"rating": 3, "date": "2024-06-01"},
+                {"rating": 4, "date": "2024-06-03"},
+            ]
+        },
+    }
+    dump_json(mock_data.MASTERED_FILE, mock_mastered_data)
+
+    mock_inprogress_data = {
+        "problem3": {
+            "history": [
+                {"rating": 5, "date": "2024-06-04"},
+                {"rating": 5, "date": "2024-06-05"},
+            ]
+        }
+    }
+    dump_json(mock_data.PROGRESS_FILE, mock_inprogress_data)
+
+    result = calendar.get_all_date_counts()
+
+    assert result["2024-06-01"] == 2  # Two history entries from mastered data
+    assert result["2024-06-02"] == 1
+    assert result["2024-06-03"] == 1
+    assert result["2024-06-04"] == 1
+    assert result["2024-06-05"] == 1
+    assert result["2024-06-06"] == 1  # audit pass
+    assert result["2024-06-08"] == 1  # audit pass
+    assert "2024-06-07" not in result  # audit fail, should not be included

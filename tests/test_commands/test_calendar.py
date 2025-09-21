a2f3729637c09d0bcfd494f8dd0427207f614712
Hayes Barber <barberhayes@gmail.com>
2025-09-21 10:43:57 -0400

    test get dates

--- tests/test_commands/test_calendar.py
+++ tests/test_commands/test_calendar.py
@@ -14,3 +14,25 @@ def test_get_audit_dates(mock_data, dump_json):
     result = calendar.get_audit_dates()
 
     assert result == ["2024-06-06", "2024-06-08"]
+
+
+def test_get_dates(mock_data, dump_json):
+    mock_mastered_data = {
+        "problem1": {
+            "history": [
+                {"rating": 3, "date": "2024-06-01"},
+                {"rating": 5, "date": "2024-06-02"},
+            ]
+        },
+        "problem2": {
+            "history": [
+                {"rating": 3, "date": "2024-06-01"},
+                {"rating": 4, "date": "2024-06-03"},
+            ]
+        },
+    }
+    dump_json(mock_data.MASTERED_FILE, mock_mastered_data)
+
+    result = calendar.get_dates(mock_data.MASTERED_FILE)
+
+    assert result == ["2024-06-01", "2024-06-02", "2024-06-01", "2024-06-03"]

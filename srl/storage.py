a53b3a040c5cb7a416224ab2dd77159c7f9a2291
Hayes Barber <barberhayes@gmail.com>
2025-11-03 19:13:03 -0500

    add types

--- srl/storage.py
+++ srl/storage.py
@@ -13,13 +13,13 @@ def ensure_data_dir():
     DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 
-def load_json(file_path: Path):
+def load_json(file_path: Path) -> dict:
     if not file_path.exists():
         return {}
     with open(file_path, "r") as f:
         return json.load(f)
 
 
-def save_json(file_path, data):
+def save_json(file_path: Path, data: dict):
     with open(file_path, "w") as f:
         json.dump(data, f, indent=2)

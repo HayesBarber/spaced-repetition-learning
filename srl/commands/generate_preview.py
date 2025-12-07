7ff7f1dbce7a1364eba0a0b2b4596a277c56abfd
HayesBarber <barberhayes@gmail.com>
2025-12-06 21:43:04 -0500

    init command

---
+++ srl/commands/generate_preview.py
@@ -0,0 +1,13 @@
+from rich.console import Console
+
+
+def add_subparser(subparsers):
+    gen_preview = subparsers.add_parser(
+        "generate-preview", help="Generate SVG preview for the README"
+    )
+    gen_preview.set_defaults(handler=handle)
+    return gen_preview
+
+
+def handle(_, console: Console):
+    pass

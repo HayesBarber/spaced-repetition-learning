70d0c420a5c2affdcc584b90293066b99fb4bdbc
Hayes Barber <barberhayes@gmail.com>
2025-12-11 17:20:14 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="srl",
-    version="0.1.0",
     author="Hayes Barber",
     packages=find_packages(),
-    install_requires=[
-        "rich",
-    ],
     entry_points={
         "console_scripts": [
             "srl = srl.main:main",

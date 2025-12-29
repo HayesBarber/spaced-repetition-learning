7a948765640a9f2605a99ad8771613f573302f02
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-12-29 09:21:09 -0800

    feat(url): add URL support for add & inprogress commands
    - added optional --url/-u flags to both add and inprogress commands
    - add URLs to problems_in_progress.json when provided via
    - users can view problem URLs with
    - problem URLs are rendered as hyperlinks with rich.markdown

--- srl/commands/inprogress.py
+++ srl/commands/inprogress.py
@@ -1,5 +1,6 @@
 from rich.console import Console
 from rich.panel import Panel
+from rich.markdown import Markdown
 from srl.storage import (
     load_json,
     PROGRESS_FILE,
@@ -8,16 +9,24 @@ from srl.storage import (
 
 def add_subparser(subparsers):
     parser = subparsers.add_parser("inprogress", help="List problems in progress")
+    parser.add_argument("-u", "--url", action="store_true", help="Include problem URLs")
     parser.set_defaults(handler=handle)
     return parser
 
 
 def handle(args, console: Console):
+    url_enabled: bool = getattr(args, "url", False)
     in_progress = get_in_progress()
     if in_progress:
         console.print(
             Panel.fit(
-                "\n".join(f"{i+1}. {p}" for i, p in enumerate(in_progress)),
+                # TODO: update tests to accept MD output
+                # meanwhile, don't render MD for default input to pass tests
+                (
+                    Markdown("\n".join(f"{i+1}. {p["name"]}{f"  [Open in LeetCode]({p["url"]})" if p["url"] else ""}" for i, p in enumerate(in_progress)))
+                    if url_enabled 
+                    else "\n".join(f"{i+1}. {p["name"]}" for i, p in enumerate(in_progress))
+                ),
                 title=f"[bold magenta]Problems in Progress ({len(in_progress)})[/bold magenta]",
                 border_style="magenta",
                 title_align="left",
@@ -31,7 +40,6 @@ def get_in_progress() -> list[str]:
     data = load_json(PROGRESS_FILE)
     res = []
 
-    for name, _ in data.items():
-        res.append(name)
-
+    for name, info in data.items():
+        res.append({"name": name, "url": info.get("url", "")})
     return res

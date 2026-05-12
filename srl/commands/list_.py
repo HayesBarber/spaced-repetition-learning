f8e1cc34f4b523d2e14f77d971147305cbd83fb4
Hayes Barber <barberhayes@gmail.com>
2026-05-11 21:20:55 -0400

    make link blue

--- srl/commands/list_.py
+++ srl/commands/list_.py
@@ -92,7 +92,7 @@ def should_audit():
 def format_problem(problem: str, problem_url: str | None):
     """Returns "problem (url)" if url is present, otherwise "problem" """
     if problem_url:
-        return f"{problem} ({problem_url})"
+        return f"{problem} ([blue]{problem_url}[/blue])"
 
     return problem
 
